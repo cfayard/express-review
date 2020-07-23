4ded6ae3dcb8dacb702eac2bdeeb08d607f54e03
Casey Fayard <fayardcasey@gmail.com>
2020-07-23 15:05:26 -0400

    using built in middleware

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const log = require("./logger");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(logger);
 
