3c7bc7665f2d3f5575d928d11f6127365b3b5962
Casey Fayard <fayardcasey@gmail.com>
2020-07-23 17:30:41 -0400

    callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,17 +1,27 @@
 const { get } = require("config");
 
 console.log("Before");
-const user = getUser(1);
+getUser(1, (user) => {
+  //   Get the repositories
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos", repos);
+  });
+});
 console.log("After");
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database...");
-    return {
+    callback({
       id: id,
       gitHubUsername: "cfayard",
-    };
+    });
   }, 2000);
+}
 
-  return 1;
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling Github API...");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
