f6e1da2807b85986d2a625cc46965607172af935
Casey Fayard <fayardcasey@gmail.com>
2020-07-24 09:58:25 -0400

    remove callbacks and use promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -6,27 +6,37 @@ function getRepositories(user) {
   getRepositories(user.gitHubUsername, getCommits);
 }
 
-function getCommits(repos) {
-  getCommits(repo, displayCommits);
-}
-
 function displayCommits(commits) {
   console.log(commits);
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({
-      id: id,
-      gitHubUsername: "cfayard",
-    });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({
+        id: id,
+        gitHubUsername: "cfayard",
+      });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("Calling Github API...");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling Github API...");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling Github API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
 }
