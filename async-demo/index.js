31b9b3e9fdd6f0ec7152682e22e4eec194dcde84
Casey Fayard <fayardcasey@gmail.com>
2020-07-25 19:48:21 -0400

    1

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,21 @@
 console.log("Before");
 // getUser(1, getRepositories);
 
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+// // Promise-based approach
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
+
+// Async and Await approach
+async function displayCommits() {
+  const user = await getUser(1);
+  const repos = await getRepositories(user.gitHubUsername);
+  const commits = await getCommits(repos[0]);
+  console.log(commits);
+}
+displayCommits();
 
 console.log("After");
 
